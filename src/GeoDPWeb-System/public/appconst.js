4f5774791be5fb21c4f75da7578b327d5d6092fe
e <805103505@qq.com>
2019-07-19 18:57:22 +0800

    GeoDPWeb_v1.1.0

--- src/GeoDPWeb-System/public/appconst.js
+++ src/GeoDPWeb-System/public/appconst.js
@@ -5,7 +5,7 @@ var geodp = geodp || {};
 
 (function() {
   geodp.appconst = geodp.appconst || {
-    version: "1.0.1",
+    version: "1.1.0",
     userManagement: {
       defaultAdminUserName: "admin"
     },
@@ -18,8 +18,11 @@ var geodp = geodp || {};
     cookieName: {
       token: "Abp.AuthToken"
     },
-    baseAPI: "http://172.18.2.251:8012",
-    baseURL: "http://172.18.2.251:8010/system",
-    authURL: "http://172.18.2.251:8010/auth"
+    // baseAPI: "http://172.18.2.251:8012",
+    // baseURL: "http://172.18.2.251:8010/system",
+    // authURL: "http://172.18.2.251:8010/auth"
+    baseAPI: "http://cdyj-s00:8013",
+    baseURL: "http://cdyj-s00:8010/system",
+    authURL: "http://cdyj-s00:8010/auth"
   };
 })();
